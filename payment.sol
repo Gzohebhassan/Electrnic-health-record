7baae9024dca5e714f1fcb88330e83300c735f2d
Gzohebhassan <88276275+Gzohebhassan@users.noreply.github.com>
2021-08-01 18:46:35 +0530

    Update payment.sol

--- payment.sol
+++ payment.sol
@@ -1,16 +1,15 @@
 pragma solidity >=0.7.0 <0.9.0;
-contract doctorpayment
+contract payment
 {
-    address public doctor;
-    address[] public patient;
+    address public admin;
+    address[] public insurance;
     constructor() public
     {
-        doctor = msg.sender;
+        admin = msg.sender;
     }
     function makepayment() public payable
     {
         require(msg.value > 0.1 ether);
-        patient.push(msg.sender);
+        insurance.push(msg.sender);
     }
     
-}
